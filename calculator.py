ccdd4913674f71aa784e9877cb443471e0c753e8
root <root@ip-172-31-18-36.us-east-2.compute.internal>
2021-03-03 05:34:27 +0000

    sample python app calculator

---
+++ calculator.py
@@ -0,0 +1,11 @@
+import math
+import requests
+
+def sqrt(item):
+    return math.sqrt(item)
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
